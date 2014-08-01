49e7a2fb4af8886d3ae81d2ad03ded74f23681f0
David Holstius <david.holstius@gmail.com>
2014-08-01 09:27:32 -0700

    Change "percentiles" to "pctl"

--- R/pctls.R
+++ R/pctls.R
@@ -4,7 +4,7 @@
 #' @param i (optional) expression indicating which values to include in the calculation 
 #' @param \dots further arguments to base::rank
 #' @export
-ranks <- function (x, i=which(x > 0), ...) {
+comp_rank <- function (x, i=which(x > 0), ...) {
   r <- rank(x[i], ties.method = "max", ...)
   replace(x, i, r)
 }
@@ -12,9 +12,9 @@ ranks <- function (x, i=which(x > 0), ...) {
 #' Calculate percentiles (as in CalEnviroScreen 2.0)
 #'
 #' @param x values
-#' @param \dots futher arguments to \link{ranks}
+#' @param \dots futher arguments to \link{comp_rank}
 #' @export
-percentiles <- function (x, ...) {
-  r <- ranks(x, ...)
+pctl <- function (x, ...) {
+  r <- comp_rank(x, ...)
   r / max(r, na.rm=TRUE) * 100
 }
